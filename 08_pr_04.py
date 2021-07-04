a0c0ca72f633bc5070cc998752be45790988589e
aadilkhhan <86908447+aadilkhhan@users.noreply.github.com>
2021-07-04 14:17:05 +0530

    Add files via upload

---
+++ 08_pr_04.py
@@ -0,0 +1,11 @@
+from os import replace
+
+
+with open ('sample.txt' ) as f:
+    content = f.read()
+
+
+content = content.replace("donkey" , "$%*@$'#''")
+
+with open ('sample.txt' , 'w') as f:
+    f.write(content)
---
+++ 14_pr_10.py
@@ -0,0 +1,4 @@
+filename = "sample.txt"
+
+with open(filename , "w") as f:
+    f.write("")
